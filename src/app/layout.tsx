c1e6b00628db91a0f57f347f80f347083a1aead2
Flow Up <flowupinova@gmail.com>
2025-09-16 01:40:00 +0000

    adicione suppressHydrationWarning  na tag body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">{children}<Toaster /></body>
+      <body className="font-body antialiased" suppressHydrationWarning>{children}<Toaster /></body>
     </html>
   );
 }
