c7efab01d57280fd3b6d018316556811061131a0
Flow Up <flowupinova@gmail.com>
2025-11-07 18:26:10 +0000

    no head adicione esse código abaixo com comentário mostrando que é refer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Poppins:wght@700&display=swap" rel="stylesheet" />
+        {/* Hotjar Tracking Code */}
+        <script src="https://t.contentsquare.net/uxa/9ce52eb57993d.js"></script>
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <Providers>
