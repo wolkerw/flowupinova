e4941941b3c3b444a44a648b522d784ee02e0f98
Flow Up <flowupinova@gmail.com>
2025-11-08 17:28:10 +0000

    Instalar o Gerenciador de tags do Google Copie o código abaixo e cole-o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Providers } from '@/components/auth/providers';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'FlowUp Chat',
@@ -20,8 +21,23 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&family=Poppins:wght@700&display=swap" rel="stylesheet" />
         {/* Hotjar Tracking Code */}
         <script src="https://t.contentsquare.net/uxa/9ce52eb57993d.js"></script>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M5LLZBRP');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5LLZBRP"
+        height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Providers>
           {children}
         </Providers>
