0251dfb266b7f73c5f6676db5e0a1aaf4333e337
Flow Up <flowupinova@gmail.com>
2026-01-21 19:54:09 +0000

    garanta que SEMPRE o projeto tenha no mínimo 90% de cobertura de linhas

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,13 @@ module.exports = {
       },
     ],
   },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
